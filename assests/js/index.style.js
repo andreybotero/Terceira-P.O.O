0dfb75a0d0937fd76f702aa3d6fc45b60055a6b1
Andrey Botero <andreybotero564@gmail.com>
2023-08-07 16:29:17 -0300

    feat: remove button

--- assests/js/index.style.js
+++ assests/js/index.style.js
@@ -1,75 +1,94 @@
 const posts = [];
 let indexPost = -1;
+let i = 0;
 
-function exibirPosts() {
-  let conteudoExibicao = "";
-  posts.forEach((post, index) => {
-    conteudoExibicao += `
-       <div class="post">
-         <h2>${post.titulo}</h2>
-         <p><strong>Resumo:</strong> ${post.resumo}</p>
-         <p><strong>Autor:</strong> ${post.autor}</p>
-         <p><strong>Data de Publicação:</strong> ${post.dataPublicacao}</p>
-         <button onclick="editarPost(${index})">Editar</button>
-         <button onclick="removerPost(${index})">Remover</button>
-       </div>
-     `;
-  });
-  document.getElementById("exibicao").innerHTML = conteudoExibicao;
-}
+function savePost() {
+    i++
+    const title = document.getElementById("title").value;
+    const resume = document.getElementById("resume").value;
+    const publisher = document.getElementById("publisher").value;
+    const date = document.getElementById("date").value;
 
-function cadastrarPost(titulo, resumo, autor, dataPublicacao) {
-  const post = {
-    titulo,
-    resumo,
-    autor,
-    dataPublicacao,
-  };
-  posts.push(post);
-  exibirPosts();
-}
-
-function editarPost(index) {
-  const post = posts[index];
-
-  document.getElementById("titulo").value = post.titulo;
-  document.getElementById("resumo").value = post.resumo;
-  document.getElementById("autor").value = post.autor;
-  document.getElementById("data_publicacao").value = post.dataPublicacao;
-
-  indexPost = index;
-}
-
-function removerPost(index) {
-  posts.splice(index, 1);
-
-  exibirPosts();
-}
-
-function savePosts() {
-  const titulo = document.getElementById("titulo").value;
-  const resumo = document.getElementById("resumo").value;
-  const autor = document.getElementById("autor").value;
-  const dataPublicacao = document.getElementById("data_publicacao").value;
-
-  document.getElementById("titulo").value = "";
-  document.getElementById("resumo").value = "";
-  document.getElementById("autor").value = "";
-  document.getElementById("data_publicacao").value = "";
-
-  if (indexPost === -1) {
-    if (titulo && resumo && autor && dataPublicacao) {
-      cadastrarPost(titulo, resumo, autor, dataPublicacao);
+    if (indexPost == -1) {
+        if (title && resume && publisher && date) {
+        storePost(title, resume, publisher, date);
+        cleanFields();
     }
-  } else {
-    if (titulo && resumo && autor && dataPublicacao) {
-      posts[indexPost] = {
-        titulo,
-        resumo,
-        autor,
-        dataPublicacao,
-      };
-      exibirPosts();
-    }
-  }
-}   
+}else {
+        if (title && resume && publisher && date) {
+            posts[indexPost] = {
+                title,
+                resume,
+                publisher,
+                date
+            }
+    }cleanFields();
+}
+
+    
+    showPosts();
+    indexPost = -1;
+
+}
+
+function storePost(title, resume, publisher, date) {
+    const post = {
+        title,
+        resume,
+        publisher,
+        date
+    };
+
+    posts.push(post)
+}
+
+function showPosts() {
+    document.getElementById("list").classList.remove("hidden");
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+        <div class="post">
+        <h2>${post.title}</h2>
+        <p><strong>Resumo: </strong>${post.resume}</p>
+        <p><strong>Autor: </strong>${post.publisher}</p>
+        <p><strong>Data de publicação: </strong>${post.date}</p>
+
+        <button onclick="editPost(${index})">Editar</button>
+        <button onclick="removePost(${index})">Remover</button>
+        
+        </div>
+        `;
+    })
+    document.getElementById("list").innerHTML = showContent;
+}
+
+function cleanFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("publisher").value = "";
+    document.getElementById("resume").value = "";
+}
+
+function editPost(index) {
+    indexPost = index;
+    const post = posts[index];
+
+    document.getElementById("title").value = post.title
+    document.getElementById("resume").value = post.resume
+    document.getElementById("publisher").value = post.publisher
+    document.getElementById("date").value = post.date
+}
+
+function removePost(index) {
+    posts.splice(index, 1);
+
+    showPosts();
+if (i == 1) {
+    document.getElementById("list").classList.add("hidden");
+}
+i = i - 1;
+        
+    
+    
+}
